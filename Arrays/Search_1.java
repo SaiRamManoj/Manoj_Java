ce53afbac469b51c9f9d937ecee10c7dcecf5645
Sai Ram Manoj Pusala <sairammanoj99@gmail.com>
2024-01-18 12:15:52 -0600

    arrays

--- Arrays/Search_1.java
+++ Arrays/Search_1.java
@@ -4,7 +4,9 @@ public class Search_1 {
         int[] arr = {34,56,12,11,45,67,78,43,323,34,43};
         int searchElement = 56;
         for(int i=0;i<arr.length;i++)
-            if(arr[i]==searchElement)
+            if(arr[i]==searchElement){
                 System.out.println("elemenent found at index "+ (i+1));
+                break;
+            }
     }
 }
