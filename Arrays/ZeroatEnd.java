f620dda93a8c354f69d5ad3701f0f9344d6fcd82
Sai Ram Manoj Pusala <sairammanoj99@gmail.com>
2024-01-18 13:17:57 -0600

    Arrrays

--- Arrays/ZeroatEnd.java
+++ Arrays/ZeroatEnd.java
@@ -1,20 +1,16 @@
 public class ZeroatEnd{
     public static void main(String[] args) {
-        int[] arr = {34,56,12,11,45,67,78,0,323,34,0};
+        int[] arr = {34,56,12,11,0,67,78,0,323,34,0};
         int somewhere =0;
+        int result[] = new int[arr.length];
         for(int i=0;i<arr.length;i++){
             if(arr[i]==0){
-                int j = i+1;
-                while(j<arr.length && arr[j]<0)
-                    j++;
-                if(j!=arr.length){
-                arr[i] = arr[j];
-                arr[j] = 0;
-                }
+                continue;
             }
+            result[somewhere++] = arr[i];
         }
 
-        for(int i=0;i<arr.length;i++)
-        System.out.print(arr[i]+" ");
+        for(int i=0;i<result.length;i++)
+        System.out.print(result[i]+" ");
     }
 }
