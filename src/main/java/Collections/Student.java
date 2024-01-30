7eddd7c5acb5240ec662b81f5840156ec11f62b5
Sai Ram Manoj Pusala <sairammanoj99@gmail.com>
2024-01-30 13:01:04 -0600

    -m

--- src/main/java/Collections/Student.java
+++ src/main/java/Collections/Student.java
@@ -5,6 +5,30 @@ public class Student {
     String lastName;
     public double gpa;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getGPA() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public Student(String firstName, String lastName, double gpa) {
         this.firstName = firstName;
         this.lastName = lastName;
