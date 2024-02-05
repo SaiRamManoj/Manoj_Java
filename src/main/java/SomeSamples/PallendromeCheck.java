71ab40f8a577503497622b6f8146a8caddc8a917
Sai Ram Manoj Pusala <sairammanoj99@gmail.com>
2024-02-05 15:41:15 -0600

    some updations

--- src/main/java/SomeSamples/PallendromeCheck.java
+++ src/main/java/SomeSamples/PallendromeCheck.java
@@ -5,7 +5,7 @@ public class PallendromeCheck {
         String s1=num1+"";
         if (s1.length()==1)
             return true;
-        for(int i=0;i<s1.length();i++)
+        for(int i=0;i<s1.length()/2;i++)
             if(s1.charAt(i) !=  s1.charAt(s1.length()-1-i))
                 return false;
         return true;
