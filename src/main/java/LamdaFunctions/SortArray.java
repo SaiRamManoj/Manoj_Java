e71666da8466cb29d1c5b7ba36aa6f9ab6023948
Sai Ram Manoj Pusala <sairammanoj99@gmail.com>
2024-02-09 15:33:09 -0600

    remaining last lambda

--- src/main/java/LamdaFunctions/SortArray.java
+++ src/main/java/LamdaFunctions/SortArray.java
@@ -2,11 +2,18 @@ package LamdaFunctions;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class SortArray {
-//    public String[] sortArray(String[] arr){
-//        return Arrays.stream(arr).sorted(Comparator.comparing(String::length)
-//                .thenComparing(Comparator.comparing(s->s.substring(s.length()-1)).reversed()))
-//                .toArray(String[]::new);
-//    }
+    public String[] sortArray(String[] arr){
+        return Arrays.stream(arr)
+                .sorted((stringOne, stringTwo) -> {
+                    if(stringOne.length() == stringTwo.length())
+                        return stringTwo.charAt(stringTwo.length() - 1)
+                                - stringOne.charAt(stringOne.length() - 1);
+                    else
+                        return stringOne.length() - stringTwo.length();
+                })
+                .toArray(String[] :: new);
+    }
 }
